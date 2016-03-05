99a631232421a24a351baf1d567bc00d6fe952c6
Nipun Ramakrishnan <nipun.ramk@berkeley.edu>
2016-03-04 16:50:15 -0800

    Initial commit

---
+++ app/src/main/java/com/example/nipunramk/androidweek3project/Car.java
@@ -0,0 +1,18 @@
+package com.example.nipunramk.androidweek3project;
+
+/**
+ * Created by nipun.ramk on 3/4/2016.
+ */
+public class Car {
+
+    public String title;
+    public String description;
+    public int imageId;
+
+    public Car(String t, String d, int imgId) {
+         title = t;
+        description = d;
+        imageId = imgId;
+
+    }
+}
